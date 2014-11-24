1e709c792b8514807f671b12acfbc715d7e982b8
Weibin <a1994218@hotmail.com>
2014-11-24 05:31:03 +0100

    green

--- src/main/HotOrCold.java
+++ src/main/HotOrCold.java
@@ -4,8 +4,12 @@ import java.util.Random;
 
 public class HotOrCold {
 	Random rand = new Random();
+	
 	public int RandomNum() {
-		int i = rand.nextInt();
+		int i = rand.nextInt(75)+1;
+		if(i < 1 || i> 75)
+			throw new IllegalArgumentException();
+		
 		return i;
 	}
 
